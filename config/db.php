c4f32e55186d20631e5d8d3e1af948a62bfc9938
IvanTsumbek <maincumbek@gmail.com>
2025-05-06 10:14:05 +0300

    The first commit

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+
+return[
+'host'=> '127.127.126.26',
+'dbname' => 'test',
+'username'=> 'root',
+'password'=>'',
+'charset'=>'utf8', //utf8mb4
+'options'=> [
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+]
+];
